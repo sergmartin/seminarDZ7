6322d571de66f365fb0ab96d178f29b8b728ae42
Сергей <sergemartin@yandex.ru>
2022-10-13 23:11:44 +0300

    решил задачу 47

---
+++ task47/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача №47
+Console.Clear();
+
+Console.Write("Введите количество строк: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] matr = new double[m, n];
+
+void MaiMartix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().NextDouble() *  10;
+            Console.Write(Math.Round(matrix[i , j], 1) + " \t");
+        }
+    Console.WriteLine();
+    }
+}
+MaiMartix(matr);
